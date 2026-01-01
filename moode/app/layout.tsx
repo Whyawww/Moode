7770da01f09c6ac10021d8033827ece3def1f941
Wahyu Aji Nusantara <152489037+Whyawww@users.noreply.github.com>
2026-01-01 16:10:14 +0700

    feat effect motion fade in

--- moode/app/layout.tsx
+++ moode/app/layout.tsx
@@ -7,8 +7,17 @@ import SoundManager from "@/components/features/audio/SoundManager";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Moode",
-  description: "Aesthetic Focus Tool",
+  title: {
+    template: "%s | Moode",
+    default: "Moode - Aesthetic Focus Tool",
+  },
+  description:
+    "Productivity reimagined for students. Mix sounds, set tasks, and flow.",
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({
